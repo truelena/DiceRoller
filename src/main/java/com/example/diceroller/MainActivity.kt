76f8c80d9ee100dc81bfb81cef4724a3641ed5c0
Elena <99732365+truelena@users.noreply.github.com>
2023-01-27 13:28:58 +0300

    Add files via upload

---
+++ src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+
+        rollButton.setOnClickListener {
+            rollDice()
+            rollDice1()
+        }
+        rollDice()
+        rollDice1()
+    }
+
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)  //изменение изображения
+        diceImage.contentDescription = diceRoll.toString()
+    }
+
+    private fun rollDice1() {
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        val diceImage1: ImageView = findViewById(R.id.imageView2)
+        val drawableResource1 = when (diceRoll1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage1.setImageResource(drawableResource1)  //изменение изображения
+        diceImage1.contentDescription = diceRoll1.toString()
+    }
+
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+}
